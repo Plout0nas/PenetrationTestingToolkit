8e9e94cfedc6e3eae102536493c45dd0c3df385b
Plout0nas <e.ploutonas@gmail.com>
2021-02-24 20:53:01 +0200

    Port scanning ip and domain names

--- PortScanner.py
+++ PortScanner.py
@@ -1,18 +1,44 @@
-#!/usr/bin/python3
-
 import socket
+from IPy import IP
 
-socketObject = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-socketObject.settimeout(5)
+start = int(input("Enter the port you want your scan to start with: "))
+end = int(input("Enter the port you want your scan to end with: "))
 
-host = input("Please enter the IP you want to scan : ")
-port = int(input("Please enter the port you want to scan: "))
+def multiple_scan(ipaddress):
+    convertedIP = check_ip(ipaddress)
+    print("\n" + "Scanning Target: " + str(ipaddress))
+    # Specifying a range of ports to scan
+    for port in range(start, end + 1):
+        port_scan(convertedIP, port)
+
+# Make the domain name of a web page to be accepted
+def check_ip(ip):
+    try:
+        IP(ip)
+        return (ip)
+    except ValueError:
+        return socket.gethostbyname(ip)
+
+timer = float(input("Enter the timer you want your scan to take for each port in seconds (the bigger the better resutls): "))
+
+# Scanning ONE port based on an IP address
+def port_scan(ipaddress, port):
+    try:
+        socketObject = socket.socket()
+        # Set the time for each port to be scanned
+        socketObject.settimeout(timer)
+        # Connect to the port
+        socketObject.connect((ipaddress, port))
+        print("The port " + str(port) + " is open!")
+    except:
+        pass
 
 
-def portScanner(port):
-    if socketObject.connect_ex((host, port)):
-        print("The port is closed")
-    else:
-        print("The port is open")
 
-portScanner(port)
+ipaddress = input("Enter the IP you want to scan, to scan more than one target split them with the '+' symbol: ")
+
+if '+' in ipaddress:
+    for ip in ipaddress.split('+'):
+        multiple_scan(ip.strip(' '))
+else:
+    multiple_scan(ipaddress)
