050dcf4e821f88d2306eaf03c41b4445ffd93566
teo <teorns30@gmail.com>
2021-04-21 04:24:11 +0300

    Gui V1.0.4

--- Gui.py
+++ Gui.py
@@ -2,6 +2,7 @@
 import sys
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5 import uic
+#from Functions import  Ui_MainWindow
 
 
 class MainWindow(QtWidgets.QMainWindow):
@@ -9,6 +10,7 @@ class MainWindow(QtWidgets.QMainWindow):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         uic.loadUi("Gui.ui", self)
+
 
     #def port_scanner(self):
 
